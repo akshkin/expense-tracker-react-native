e02a204c410ae43d18c86adbd58eba2afe4678f1
akshkin <107682289+akshkin@users.noreply.github.com>
2023-07-11 10:53:51 +0200

    Add feature to set max expense limit
    Schedule a notification when expense sum exceeds set expense limit

--- screens/RecentExpenses.jsx
+++ screens/RecentExpenses.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import ExpensesOutput from "../components/ExpensesOutput";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -11,16 +11,27 @@ import { getDateMinusDays, getFormattedDate } from "../utils/date";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import ErrorOverlay from "../components/ui/ErrorOverlay";
 import Wrapper from "../components/Wrapper";
+import * as Notifications from "expo-notifications";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Button from "../components/ui/Button";
+import { StyleSheet, TextInput, View } from "react-native";
+import { GlobalStyles } from "../constants/styles";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldSetBadge: false,
+    shouldPlaySound: false,
+    shouldShowAlert: true,
+  }),
+});
 
 function RecentExpenses({ navigation }) {
+  const [maxLimit, setMaxLimit] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
   const expenses = useSelector(selectExpense);
   const isLoading = useSelector(loadingState);
   const error = useSelector(errorState);
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getExpensesFromStorage());
-  }, []);
 
   const recentExpenses = expenses?.filter((expense) => {
     const today = new Date();
@@ -28,6 +39,48 @@ function RecentExpenses({ navigation }) {
     const formattedDate7daysAgo = getFormattedDate(date7daysAgo);
     return expense?.date > formattedDate7daysAgo;
   });
+
+  const expensesSum = recentExpenses?.reduce(
+    (acc, current) => acc + current?.amount,
+    0
+  );
+
+  function scheduleNotifications() {
+    console.log("Scheduling notifications");
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Expense Tracker",
+        body: "Expense amount exceeded set limit",
+      },
+      trigger: {
+        seconds: 5,
+      },
+    });
+  }
+
+  useEffect(() => {
+    dispatch(getExpensesFromStorage());
+  }, []);
+
+  useEffect(() => {
+    async function getLimit() {
+      const limit = await AsyncStorage.getItem("limit");
+      return parseFloat(limit);
+    }
+    async function checkExpenseLimit() {
+      const limit = await getLimit();
+      if (expensesSum > limit) {
+        scheduleNotifications();
+      }
+    }
+
+    checkExpenseLimit();
+  }, [expensesSum]);
+
+  async function saveExpenseLimit() {
+    setIsOpen(false);
+    await AsyncStorage.setItem("limit", maxLimit);
+  }
 
   if (error && !isLoading) return <ErrorOverlay error={error} />;
 
@@ -37,11 +90,56 @@ function RecentExpenses({ navigation }) {
         <LoadingOverlay />
       ) : (
         <Wrapper>
+          <Button
+            style={{ marginHorizontal: 16 }}
+            onPress={() => setIsOpen(true)}
+          >
+            Set max expense limit
+          </Button>
+          {isOpen ? (
+            <View style={styles.limitContainer}>
+              <TextInput
+                style={styles.textInput}
+                keyboardType="decimal-pad"
+                value={maxLimit}
+                onChangeText={(value) => setMaxLimit(value)}
+              />
+              <View style={styles.buttonContainer}>
+                <Button
+                  style={{
+                    width: "49%",
+                  }}
+                  onPress={() => setIsOpen(false)}
+                >
+                  Cancel
+                </Button>
+                <Button style={{ width: "49%" }} onPress={saveExpenseLimit}>
+                  Save
+                </Button>
+              </View>
+            </View>
+          ) : null}
           <ExpensesOutput expenses={recentExpenses} period="Last 7 days" />
         </Wrapper>
       )}
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  textInput: {
+    borderWidth: 1,
+    padding: 8,
+    margin: 8,
+    borderRadius: 5,
+  },
+  limitContainer: {
+    marginHorizontal: 16,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    gap: 8,
+  },
+});
 
 export default RecentExpenses;
